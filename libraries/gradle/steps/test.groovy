928abec1ceafdd6a23aa0198615046a876066df5
Tushar Sisode <50324286+tusharsisode@users.noreply.github.com>
2021-06-03 17:58:18 +1000

    Update test.groovy

--- libraries/gradle/steps/test.groovy
+++ libraries/gradle/steps/test.groovy
@@ -1,1 +1,11 @@
+package gradle
 
+void call() {
+  println "Testing the code"
+  sh(
+    label: "Run gradlew tasks 'test'",
+    script: "gradlew test"
+    )
+  junit 'build/test-results/test/**/TEST-*.xml'
+  junit 'build/test-results/test/**/TEST-*.xml'
+}
