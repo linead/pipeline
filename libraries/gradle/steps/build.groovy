3b6c2720376dbdf0086e43d40165901ff81916d2
Tushar Sisode <50324286+tusharsisode@users.noreply.github.com>
2021-06-03 17:54:55 +1000

    Update build.groovy

--- libraries/gradle/steps/build.groovy
+++ libraries/gradle/steps/build.groovy
@@ -3,7 +3,7 @@ package gradle
 void call() {
   println "Building the code"
   sh(
-    label: "Run gradlew tasks 'clean bootwar testclasses'",
+    label: "Run gradlew tasks 'clean bootjar testclasses'",
     script: "gradlew clean bootwar testclasses"
     )
 }
