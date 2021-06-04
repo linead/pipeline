dd086c6d8186a63e4c34246f17d89c76e209d261
Tushar Sisode <50324286+tusharsisode@users.noreply.github.com>
2021-06-04 15:18:48 +1000

    Update code_build.groovy

--- libraries/gradle/steps/code_build.groovy
+++ libraries/gradle/steps/code_build.groovy
@@ -2,7 +2,7 @@ package gradle
 
 void call() {
   println "Building the code"
-  sh(
+  bat(
     label: "Run gradlew tasks 'clean bootjar testclasses'",
     script: "gradlew clean bootwar testclasses"
     )
