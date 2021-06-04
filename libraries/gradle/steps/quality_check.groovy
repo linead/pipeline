0a01909dd87c288d7cf7919c88833bc46cf4f13c
Tushar Sisode <50324286+tusharsisode@users.noreply.github.com>
2021-06-04 17:58:26 +1000

    Update quality_check.groovy

--- libraries/gradle/steps/quality_check.groovy
+++ libraries/gradle/steps/quality_check.groovy
@@ -1,1 +1,9 @@
+package gradle
 
+void call() {
+  println "Quality Checking the code"
+  bat(
+    label: "Run gradlew tasks 'spotbugsMain spotbugsTest'",
+    script: "gradlew spotbugsMain spotbugsTest"
+    )
+}
