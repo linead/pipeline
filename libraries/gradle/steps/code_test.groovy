9bd1ce6563fe566611d1affb597a961557c57069
Tushar Sisode <50324286+tusharsisode@users.noreply.github.com>
2021-06-07 15:15:50 +1000

    Update code_test.groovy

--- libraries/gradle/steps/code_test.groovy
+++ libraries/gradle/steps/code_test.groovy
@@ -7,7 +7,7 @@ void call() {
     script: "gradlew test componentTest"
     )
   junit 'build/test-results/test/**/TEST-*.xml'
-  zip zipFile: 'unit-test-results', dir: 'build/test-results/test/**', archive: true
+  zip zipFile: 'unit-test-results.zip', dir: 'build/test-results/test/', archive: true
   junit 'build/test-results/componentTest/**/TEST-*.xml'
-  zip zipFile: 'component-test-results', dir: 'build/test-results/componentTest/**', archive: true
+  zip zipFile: 'component-test-results.zip', dir: 'build/test-results/componentTest/', archive: true
 }
