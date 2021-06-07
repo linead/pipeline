9964cdde9808630381556da3d740e2fbc150e183
Tushar Sisode <50324286+tusharsisode@users.noreply.github.com>
2021-06-07 15:09:42 +1000

    added component Tests

--- libraries/gradle/steps/code_test.groovy
+++ libraries/gradle/steps/code_test.groovy
@@ -4,9 +4,10 @@ void call() {
   println "Testing the code"
   bat(
     label: "Run gradlew tasks 'test'",
-    script: "gradlew test"
+    script: "gradlew test componentTest"
     )
   junit 'build/test-results/test/**/TEST-*.xml'
-  stash name: 'test-results', includes: 'build/test-results/**'
-  junit 'build/test-results/test/**/TEST-*.xml'
+  zip zipFile: 'unit-test-results', dir: 'build/test-results/test/**', archive: true
+  junit 'build/test-results/componentTest/**/TEST-*.xml'
+  zip zipFile: 'component-test-results', dir: 'build/test-results/componentTest/**', archive: true
 }
