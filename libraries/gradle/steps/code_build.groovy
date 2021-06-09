bc7fc9199a91db43b6f624aef7d556715e5cb956
Daniel Fullarton <linead@gmail.com>
2021-06-09 19:54:16 +1000

    Update code_build.groovy

--- libraries/gradle/steps/code_build.groovy
+++ libraries/gradle/steps/code_build.groovy
@@ -2,10 +2,6 @@ package gradle
 
 void call() {
   println "Building the code"
-  bat(
-    label: "Run gradlew tasks 'clean bootJar testclasses'",
-    script: "gradlew clean bootJar testclasses"
-    )
   
   String img = "sample"
   add_unmanaged_img("${img}","${img}")
