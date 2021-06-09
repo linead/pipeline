20b28fd88c74a503d5f6bf18ae75bcd08c46a51e
Tushar Sisode <50324286+tusharsisode@users.noreply.github.com>
2021-06-09 16:14:44 +1000

    Update code_build.groovy

--- libraries/gradle/steps/code_build.groovy
+++ libraries/gradle/steps/code_build.groovy
@@ -6,4 +6,15 @@ void call() {
     label: "Run gradlew tasks 'clean bootJar testclasses'",
     script: "gradlew clean bootJar testclasses"
     )
+  
+  String img = "sample"
+  add_unmanaged_img("${img}","${img}")
+}
+
+def add_unmanaged_img (id, name) {
+  config.imgs = config.imgs ?: [:]
+  config.imgs[id] = [
+    name: name,
+    unmanaged: true
+  ]
 }
